5a9b15840ab3b14203e4b68980eff48ac93c2b92
Avinash kaur <avinash.kaur@vinsol.com>
2012-11-06 15:57:48 +0530

    Indentation fixed.

--- select_three_ex2/sc1.js
+++ select_three_ex2/sc1.js
@@ -2,25 +2,25 @@ var days = document.getElementsByClassName("day"), i, count = 0, text = "";
 
 function select_checkbox() {
   document.getElementById("uncheck").checked = false;
-	if (this.checked == true) {
-	  count = count + 1;
-	}
-	else if (this.checked == false) {
-	  count = count - 1;
-	}
+  if (this.checked == true) {
+    count = count + 1;
+  }
+  else if (this.checked == false) {
+    count = count - 1;
+  }
     
   if (count > 3) {
     this.checked = false;
     count--;
     for (i = 0; i < days.length; i++) {
-	    if (days[i].checked) {
-			  text = text + " " + days[i].value;	
-		  }
+      if (days[i].checked) {
+        text = text + " " + days[i].value;  
+      }
     }
-		alert("you can select only 3 values.You have already selected:" + text);
-		text = "";
+    alert("you can select only 3 values.You have already selected:" + text);
+    text = "";
   }
-	
+  
 }
 
 function select_none() {
@@ -32,5 +32,5 @@ function select_none() {
 }
 
 for (i = 0; i < days.length; i++) {
-    days[i].onclick = select_checkbox;
+  days[i].onclick = select_checkbox;
 }
