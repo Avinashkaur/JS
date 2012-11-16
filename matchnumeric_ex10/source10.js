f629dba6b0beb7ce4cc7b314694c80c6508f6841
Avinash kaur <avinash.kaur@vinsol.com>
2012-11-16 14:28:29 +0530

    revised

--- matchnumeric_ex10/source10.js
+++ matchnumeric_ex10/source10.js
@@ -1,13 +1,12 @@
 function initial() {
   var textfield1 = document.getElementById("text1");
   var textfield2 = document.getElementById("text2");
-  var button = document.getElementById("submitform");
- // button.disabled = true;
-  
+    
   textfield1.onchange = function() {
     var pattern = /^\d+(\.\d+)?$/;
     if (pattern.test(textfield1.value)) {
-      button.disabled = false;
+      textfield2.value = "true";
+      //button.disabled = false;
     }
     else {
       textfield2.value = "false";
@@ -16,20 +15,20 @@ function initial() {
   }
 }
 function validate() {
-  var oForm = document.forms[0];
-  var EventUtil = new Object();
+  var myForm = document.forms[0];
   var textfield1 = document.getElementById("text1");
   var textfield2 = document.getElementById("text2");
   var pattern = /^\d+(\.\d+)?$/;
-  //var oEvent = EventUtil.getEvent();
+
   if (pattern.test(textfield1.value)) {
-      textfield2.value = "true";
-      oForm.submit();
+      // textfield2.value = "true";
+      myForm.submit();
       //return true;
     }
   else {
-    alert("form cannot be submitted");
+    alert("Form cannot be submitted");
     return false;
     
   } 
+  //myForm.submit();
 }
