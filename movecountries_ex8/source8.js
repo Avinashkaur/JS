4bd9a19d761eef9cb66abc897c0ec77445392e59
Avinash kaur <avinash.kaur@vinsol.com>
2012-11-19 12:59:12 +0530

    error_fixed

--- movecountries_ex8/source8.js
+++ movecountries_ex8/source8.js
@@ -1,7 +1,15 @@
 function alter(listbox1,listbox2) {
-  while (listbox1.selectedIndex) {
-    var newoption = listbox1.options[listbox1.selectedIndex];
-    listbox2.options.add(newoption);
+  var newoption,newnode;
+  while (listbox1.selectedIndex != -1) {
+    console.log(listbox1.selectedIndex);
+    newoption = listbox1.options[listbox1.selectedIndex];
+    newnode = document.createElement('option');
+    newnode.value = newoption.value;
+    newnode.text = newoption.value;
+    //   // text_newnode = document.createTextNode(newoption.value);
+    //   // newnode.appendChild(text_newnode);
+    //   // listbox2.appendChild(newnode);
+    listbox2.options.add(newoption, null);
   }
 }
 
