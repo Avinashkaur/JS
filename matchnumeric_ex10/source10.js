7d1874a37d9eff746bee511af12e1da42fe19c56
Avinash kaur <avinash.kaur@vinsol.com>
2012-11-21 10:54:45 +0530

    ambiguity_removed

--- matchnumeric_ex10/source10.js
+++ matchnumeric_ex10/source10.js
@@ -1,34 +1,16 @@
-function initial() {
-  var textfield1 = document.getElementById("text1");
-  var textfield2 = document.getElementById("text2");
-    
-  textfield1.onchange = function() {
-    var pattern = /^([+-]?\d+([eE]\+?\-?\d+)?)(\.\d+)?$/;
-    if (pattern.test(textfield1.value)) {
-      textfield2.value = "true";
-      //button.disabled = false;
-    }
-    else {
-      textfield2.value = "false";
-      //button.disabled = true;
-    }
-  }
-}
+var myForm = document.getElementById("form1");
+var textfield1 = document.getElementById("text1");
+var textfield2 = document.getElementById("text2");
+var pattern = /^([+-]?\d+([eE]\+?\-?\d+)?)(\.\d+)?$/;
 function validate() {
-  var myForm = document.getElementById("form1");
-  var textfield1 = document.getElementById("text1");
-  var textfield2 = document.getElementById("text2");
-  var pattern = /^([+-]?\d+([eE]\+?\-?\d+)?)(\.\d+)?$/;
-
   if (pattern.test(textfield1.value)) {
-      // textfield2.value = "true";
-      myForm.submit();
-      //return true;
-    }
+    textfield2.value = "true";
+    myForm.submit();
+  }
   else {
+    textfield2.value = "false";
     alert("Form cannot be submitted");
     return false;
-    
-  } 
-  //myForm.submit();
+  }
 }
+  
