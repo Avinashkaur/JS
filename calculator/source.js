c8cbc0356a37cfa0297c8cc06d31e8db1587a2e9
Avinash kaur <avinash.kaur@vinsol.com>
2012-11-23 11:11:07 +0530

    revised

--- calculator/source.js
+++ calculator/source.js
@@ -1,44 +1,61 @@
 var number = "";
+var display_screen = document.getElementById('textfield');
+function display_num(num) {
+  try {
+    if ((display_screen.value != "Error") && (display_screen.value != "Infinity")) {
+      display_screen.value = display_screen.value + num;
+    }
+    else {
+      display_screen.value = num;
+    }
+    if (num == 'clear') {
+      display_screen.value = "";
+    }
+  }
+  catch (exception) {
+    display_screen.value = "";
+  }
+}
 function equals() {
-  if(form1.textfield.value == "")
-  {
+  if(display_screen.value == "") {
     return;
   }
   try {
-    form1.textfield.value = eval(form1.textfield.value);
+    display_screen.value = eval(display_screen.value);
   }
   catch(exception) {
-    form1.textfield.value = "Error";
+    display_screen.value = "Error";
   }
 }
 function sign() {
-  form1.textfield.value = '(-' + form1.textfield.value + ')';
+  display_screen.value = '(-' + display_screen.value + ')';
 }
 function mem_add() {
   if (number == "") {
-    number = number + form1.textfield.value;
-    form1.textfield.value = "";
+    number = number + display_screen.value;
+    display_screen.value = "";
   }
   else {
-    number = number + '+' + form1.textfield.value;
-    form1.textfield.value = "";
+    number = number + '+' + display_screen.value;
+    display_screen.value = "";
   }
 }
+
 function mem_subtract() {
   if (number == "") {
-    number = number + form1.textfield.value;
-    form1.textfield.value = "";
+    number = number + display_screen.value;
+    display_screen.value = "";
   }
   else {
-    number = number + '-' + '(' + form1.textfield.value + ')';
-    form1.textfield.value = "";
+    number = number + '-' + '(' + display_screen.value + ')';
+    display_screen.value = "";
   }
   
 }
 function mem_get() {
-  form1.textfield.value = number;
+  display_screen.value = number;
 }
 function mem_clear() {
   number = "";
-  form1.textfield.value = number;
+  display_screen.value = number;
 }
